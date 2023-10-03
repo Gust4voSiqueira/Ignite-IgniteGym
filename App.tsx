1a2b4c8854a2366cc28cde0cf11e6eefb5f77caf
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2023-10-03 16:23:48 -0300

    Adicionando novas configurações

--- App.tsx
+++ App.tsx
@@ -1,28 +1,29 @@
 /* eslint-disable camelcase */
 import { StatusBar } from 'react-native'
 import { NativeBaseProvider } from 'native-base'
-
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
-import { Loading } from '@components/Loading'
-import { Theme } from './src/theme'
+
 import { Routes } from '@routes/index'
+
+import { Theme } from './src/theme'
+
+import { Loading } from '@components/Loading'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
     <NativeBaseProvider theme={Theme}>
-      {fontsLoaded ? <Routes /> : <Loading />}
-
       <StatusBar
-        barStyle={'light-content'}
+        barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   )
 }
