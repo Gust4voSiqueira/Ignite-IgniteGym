82062d3a256bc37fe603eb25f8a70b69ec996723
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2023-11-03 09:38:07 -0300

    Implementando OneSignal

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,13 +5,46 @@ import { AuthRoutes } from './auth.routes'
 import { useAuth } from '@hooks/useAuth'
 import { AppRoutes } from './app.routes'
 import { Loading } from '@components/Loading'
+import { useEffect, useState } from 'react'
+import { OSNotification, OneSignal } from 'react-native-onesignal'
+import { Notification } from '@components/Notification'
+
+const linking = {
+  prefixes: ['ignitegym://', 'com.gustavo.siqueira8.ignitegym://'],
+  config: {
+    screens: {
+      exercise: {
+        path: 'exercise/:exerciseId',
+        parse: {
+          exerciseId: (exerciseId: string) => exerciseId,
+        },
+      },
+    },
+  },
+}
 
 export function Routes() {
   const { colors } = useTheme()
+  const [notification, setNotification] = useState<OSNotification>()
   const { user, isLoadingStorageData } = useAuth()
 
   const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
+
+  useEffect(() => {
+    const unsubscribe = OneSignal.Notifications.addEventListener(
+      'foregroundWillDisplay',
+      (event) => {
+        event.preventDefault()
+
+        event.getNotification().display()
+
+        setNotification(event.getNotification())
+      },
+    )
+
+    return () => unsubscribe
+  }, [])
 
   if (isLoadingStorageData) {
     return <Loading />
@@ -19,8 +52,14 @@ export function Routes() {
 
   return (
     <Box flex={1} bg={colors.gray[700]}>
-      <NavigationContainer theme={theme}>
+      <NavigationContainer theme={theme} linking={linking}>
         {user.id ? <AppRoutes /> : <AuthRoutes />}
+        {notification?.title && (
+          <Notification
+            data={notification}
+            onClose={() => setNotification(undefined)}
+          />
+        )}
       </NavigationContainer>
     </Box>
   )
